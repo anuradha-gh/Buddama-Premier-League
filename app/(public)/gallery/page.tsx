7fd0051a0899999c4f7f4e65718799f42a13c88b
anuradhalbandara <anuradhalakshmanbandara2022@gmail.com>
2025-12-19 18:31:54 +0530

    Fix JSX syntax error in gallery page

--- app/(public)/gallery/page.tsx
+++ app/(public)/gallery/page.tsx
@@ -195,7 +195,7 @@ export default function GalleryPage() {
                             </div>
                         )}
                     </div>
-                )
+                )}
 
                 {/* Lightbox Modal */}
                 {selectedImage && (
